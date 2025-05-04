34f4d62f0ac8bdee17ed01f7dad1065187c8e206
ProPandaMen <propandamen@gmail.com>
2025-05-05 00:00:18 +0300

    fix navbar

--- custa/renderer/elements/nav/nav_button.py
+++ custa/renderer/elements/nav/nav_button.py
@@ -2,10 +2,12 @@ from custa.renderer.base import ElementRenderer
 
 class NavButtonRenderer(ElementRenderer):
     def render(self, node, render_children):
-        
+        text = node.props.get("text", "")
+        icon = node.props.get("icon", "")
+
         return f'''
             <div class="nav-button">
-                <i class="fas fa-palette"></i>
-                <span>Your Work</span>
+                <i class="{icon}"></i>
+                <span>{text}</span>
             </div>
         '''
--- custa/renderer/elements/nav/navbar.py
+++ custa/renderer/elements/nav/navbar.py
@@ -4,37 +4,23 @@ class NavbarRenderer(ElementRenderer):
     def render(self, node, render_children):
         title = node.props.get("title", "")
 
-        content = render_children(node.children)
-        
-        title_block = f"""
-        <div id="nav-header">
-            <a id="nav-title" href="/" target="_blank">{title}</a>
-            <label for="nav-toggle"><span id="nav-toggle-burger"></span></label>
-            <hr />
-        </div>
-        """ if title else ""
+        footer_nodes = [child for child in node.children if child.type == "nav_footer"]
+        content_nodes = [child for child in node.children if child.type != "nav_footer"]
+
+        content_html = render_children(content_nodes)
+        footer_html = render_children(footer_nodes)
         
         return f'''
-        {title_block}
-
-        <div id="nav-content">
-            {content}
-        </div>
-
-        <div id="nav-footer">
-            <div id="nav-footer-heading">
-                <div id="nav-footer-avatar">
-                    <img src="https://gravatar.com/avatar/4474ca42d303761c2901fa819c4f2547" />
-                </div>
-
-                <div id="nav-footer-titlebox">
-                    <div id="nav-footer-title">uahnbu</div>
-                    <span id="nav-footer-subtitle">Admin</span>
-                </div>
-
-                <button id="logout-button" title="Logout">
-                    <i class="fas fa-sign-out-alt"></i>
-                </button>
+            <div id="nav-header">
+                <a id="nav-title" href="/" target="_blank">{title}</a>
+                <label for="nav-toggle"><span id="nav-toggle-burger"></span></label>
             </div>
-        </div>
+
+            <div id="nav-content">
+                <hr />
+                {content_html}
+                <div id="nav-content-highlight"></div>
+            </div>
+
+            {footer_html}
         '''
