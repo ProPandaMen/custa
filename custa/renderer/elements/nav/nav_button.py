43ea56d9a956f93c93792648a3b2e253e3891402
Ilya Nikiforov <propandamen@gmail.com>
2025-05-14 23:29:56 +0300

    add nav button link

--- custa/renderer/elements/nav/nav_button.py
+++ custa/renderer/elements/nav/nav_button.py
@@ -4,10 +4,11 @@ class NavButtonRenderer(ElementRenderer):
     def render(self, node, render_children):
         text = node.props.get("text", "")
         icon = node.props.get("icon", "")
+        link = node.props.get("link", "")
 
         return f'''
-            <div class="nav-button">
+            <a href="{link}" class="nav-button">
                 <i data-lucide="{icon}"></i>
                 <span>{text}</span>
-            </div>
+            </a>
         '''
